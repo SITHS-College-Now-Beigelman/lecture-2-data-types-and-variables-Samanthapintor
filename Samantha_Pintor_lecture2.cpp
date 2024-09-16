bb8cbeda99ff150c88a4f9136e2109f5724f705f
Samanthapintor <samanthap329@nycstudents.net>
2024-09-16 16:37:27 -0400

    Commit

---
+++ Samantha_Pintor_lecture2.cpp
@@ -0,0 +1,36 @@
+//Samantha Pintor
+//Lecture 2 
+//9-16-2024
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+
+{
+    // Creating variables
+    int myNum = 7;
+    float myFloatNum = 7.77;
+    double myDouNum = 7.70;
+    char myLetter = 'Q';
+    bool myBoolean = true; 
+    string myString = "hello";
+
+    //print variable values 
+    cout << "int:" << myNum << "\n";
+    cout << "float:" << myFloatNum << "\n";
+    cout << "double:" << myDouNum << "\n";
+    cout << "char:" << myLetter << "\n";
+
+    //performing the expression 2 + 3.5
+    int MyNum; 
+    MyNum = 2;
+    float MyFloatNum;
+    MyFloatNum = 3.5;
+
+    cout << MyNum << "MyNum = "; 
+    cout << MyNum + 3.5 << "MyFloatNum and";
+    
+    return 0; 
+}
