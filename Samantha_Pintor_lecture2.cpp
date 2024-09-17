cbec611f9847035280e77b2d480ebcd483831763
Samanthapintor <samanthap329@nycstudents.net>
2024-09-17 15:57:49 -0400

    Comitt

--- Samantha_Pintor_lecture2.cpp
+++ Samantha_Pintor_lecture2.cpp
@@ -12,25 +12,27 @@ int main ()
     // Creating variables
     int myNum = 7;
     float myFloatNum = 7.77;
-    double myDouNum = 7.70;
+    double myDouNum = 7.71;
     char myLetter = 'Q';
     bool myBoolean = true; 
     string myString = "hello";
 
     //print variable values 
-    cout << "int:" << myNum << "\n";
-    cout << "float:" << myFloatNum << "\n";
-    cout << "double:" << myDouNum << "\n";
-    cout << "char:" << myLetter << "\n";
+    cout << "int:" << myNum << endl;
+    cout << "float:" << myFloatNum << endl;
+    cout << "double:" << myDouNum << endl;
+    cout << "char:" << myLetter << endl;
 
-    //performing the expression 2 + 3.5
-    int MyNum; 
-    MyNum = 2;
-    float MyFloatNum;
-    MyFloatNum = 3.5;
+   //comparing the variables
+   char char1 = 'A';
+   char char2 = 'B'; 
+   cout << "A < B = " << (char1<char2) <<endl; 
+   
+   //performing the operations  
+    cout << "2 + 3.5 = " << 2 + 3.5 << endl;
+    cout << "6/4 + 3.9 = " << 6/4 + 3.9 << endl;
+    cout << "5.4 * 2 -(13.6 + 18/2) = " << 5.4 * 2 -(13.6 + 18/2) << endl;
 
-    cout << MyNum << "MyNum = "; 
-    cout << MyNum + 3.5 << "MyFloatNum and";
     
     return 0; 
 }
